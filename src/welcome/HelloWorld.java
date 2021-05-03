a7291b6ec84cb2fc73e642c7067f39c0ade4d2eb
Mapala1965 <mapalamegha@gmail.com>
2021-05-03 01:04:57 -0400

    second commit

--- src/welcome/HelloWorld.java
+++ src/welcome/HelloWorld.java
@@ -5,6 +5,14 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Git project ........|||||||>>>>....");
+		
+		int [] a= {2,3,4,5,};
+		
+		System.out.println(a.length);
+		
+		for (int i=0; i<4; i++) {
+			System.out.println(a[i]) ;
+		}
 
 	}
 
